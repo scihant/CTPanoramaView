3bd33e704b95ea51b7998bf45362beb1587f9248
Cihan Tek <cihantek@gmail.com>
2016-10-11 22:23:14 +0300

    Initial development

---
+++ CTPanoramaController/CTPanoramaController.swift
@@ -0,0 +1,100 @@
+//
+//  CTPanoramaController
+//  CTPanoramaController
+//
+//  Created by Cihan Tek on 11/10/16.
+//  Copyright © 2016 Home. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+
+@objc public enum CTPanaromaControlMethod: Int {
+    case Accelerometer
+    case Touch
+}
+
+@objc public class CTPanoramaController: UIViewController {
+
+    @IBOutlet weak var sceneView: SCNView!
+    
+    override public var shouldAutorotate: Bool {
+        return true
+    }
+    
+    override public var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .all
+    }
+    
+    override public var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    public var controlMethod = CTPanaromaControlMethod.Accelerometer
+    private let cameraNode = SCNNode()
+    private var prevLocation = CGPoint.zero
+    
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+  
+        let camera = SCNCamera()
+        camera.zFar = 100
+        camera.xFov = 70
+        camera.yFov = 70
+        cameraNode.camera = camera
+        
+        let material = SCNMaterial()
+        let texture = UIImage(named: "01.png")
+        material.diffuse.contents = texture
+        material.diffuse.mipFilter = .nearest
+        material.diffuse.magnificationFilter = .nearest
+        material.diffuse.contentsTransform = SCNMatrix4MakeScale(-1, 1, 1)
+        material.diffuse.wrapS = .repeat
+        material.cullMode = .front
+
+        let sphere = SCNSphere(radius: 50)
+        sphere.segmentCount = 300
+        sphere.firstMaterial = material
+        
+        let sphereNode = SCNNode()
+        sphereNode.geometry = sphere
+        
+        sphereNode.position = SCNVector3Make(0, 0, 0)
+        cameraNode.position = sphereNode.position
+        
+        let scene = SCNScene()
+        
+        scene.rootNode.addChildNode(cameraNode)
+        scene.rootNode.addChildNode(sphereNode)
+
+        sceneView.scene = scene
+        sceneView.backgroundColor = UIColor.black
+        
+        if (controlMethod == .Touch) {
+            let panGestureRec = UIPanGestureRecognizer(target: self, action: #selector(handlePan(panRec:)))
+            sceneView.addGestureRecognizer(panGestureRec)
+        }
+        else {
+            
+        }
+    }
+    
+    @objc private func handlePan(panRec: UIPanGestureRecognizer) {
+        if (panRec.state == .began) {
+            prevLocation = CGPoint.zero
+        }
+        else if (panRec.state == .changed) {
+            let location = panRec.translation(in: sceneView)
+            print("\(location)")
+            let orientation = cameraNode.eulerAngles
+            let newOrientation = SCNVector3Make(orientation.x + Float(location.y - prevLocation.y) * 0.005,
+                                                orientation.y + Float(location.x - prevLocation.x) * 0.005,
+                                                orientation.z)
+            
+            cameraNode.eulerAngles = newOrientation
+            prevLocation = location
+        }
+    }
+    
+}
+
